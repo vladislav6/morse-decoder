0d7ae03d769f4c612b9967b1ff79e053eb7e96c6
Vladislav <vlad.kvan96@gmail.com>
2026-05-09 00:43:47 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,35 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  function makeChunks(str, s, e, chunkSize) {
+    const chunks = [];
+    let start = s;
+    let end = e;
+    for (let i = 0; i < str.length; i += 1) {
+      if (str[start]) {
+        chunks.push(str.slice(start, end));
+      }
+      start += chunkSize;
+      end += chunkSize;
+    }
+    return chunks;
+  }
+  const chunks = makeChunks(expr, 0, 10, 10);
+  let result = [];
+  chunks.map((chunk) => {
+    if (chunk === '**********') {
+      result += ' ';
+    } else {
+      result += makeChunks(chunk, 0, 2, 2)
+        .filter((f) => f !== '00')
+        .map((m) => (m === '10' ? '.' : '-'))
+        .join('')
+        .split()
+        .map((m) => MORSE_TABLE[m]);
+    }
+    return result;
+  });
+  return result;
 };
